fe67c474ddcebb1e90ed712333c6771bfd66ba8d
DJmong <49215689+DJmong@users.noreply.github.com>
2020-06-03 16:44:14 +0900

    Motor, TracerSensor 코드 최적화, 주석 추가, 핀 매핑
    아두이노 메가 -> 아두이노 나노 핀으로 매핑
    Signal.h 관련 더미 데이터 삭제
    각 함수, 변수, 매크로에 대한 주석 추가

---
+++ Motor.cpp
@@ -0,0 +1,72 @@
+#include "Motor.h"
+
+//매크로 정의
+#define set_bit(value, bit) ( _SFR_BYTE(value) |=  _BV(bit))
+#define clear_bit(value, bit) ( _SFR_BYTE(value) &= ~_BV(bit))
+
+
+//모터 핀 매크로 정의
+#define L_PWM OCR0A // pin12
+#define R_PWM OCR2A // pin6
+#define MOTOR PORTB
+#define MOTOR_INIT DDRB
+//모터 회전 방향 매크로 정의
+#define L_BACK      (set_bit(MOTOR, 3));\
+                    (clear_bit(MOTOR, 2));
+
+#define L_FORWARD   (set_bit(MOTOR, 2));\
+                    (clear_bit(MOTOR, 3));
+                    
+#define R_BACK      (set_bit(MOTOR, 1));\
+                    (clear_bit(MOTOR, 0));
+
+#define R_FORWARD   (set_bit(MOTOR, 0));\
+                    (clear_bit(MOTOR, 1));
+
+//진행 방향 매크로 정의
+#define STOP (set_bit(MOTOR, 0));\
+             (set_bit(MOTOR, 1));\
+             (set_bit(MOTOR, 2));\
+             (set_bit(MOTOR, 3));
+
+#define GO L_FORWARD;\
+           R_FORWARD;
+
+//모터 초기설정
+void MotorInit() {
+  MOTOR_INIT = 0x0F; // motor control, output
+  TCCR0A |= (1 << WGM01) | (1 << WGM00);
+  TCCR0A |= (1 << COM0A1) | (0 << COM0A0);
+  TCCR0B |= (0 << CS02) | (0 << CS01) | (1 << CS00);
+
+  TCCR2A |= (1 << WGM01) | (1 << WGM00);
+  TCCR2A |= (1 << COM2A1) | (0 << COM2A0);
+  TCCR2B |= (0 << CS02) | (0 << CS01) | (1 << CS00);
+
+  GO;
+}
+
+//모터 입력
+void MotorIn(int L_speed, int R_speed) {
+  if(L_speed == 125) L_speed = 0;
+  else if(L_speed > 255)L_speed = 255;
+  else if(L_speed < 125){
+    if(L_speed < 0) L_speed = 0;
+    L_BACK;
+    L_speed = 255 - L_speed;
+  }
+  else L_FORWARD;
+  
+  if(R_speed == 125) R_speed = 0;
+  else if(R_speed > 255) R_speed = 255;
+  else if(R_speed < 125){
+    if(R_speed < 0) R_speed = 0;
+    R_BACK;
+    R_speed = 255 - R_speed;
+  }
+  else R_FORWARD;
+   
+  L_PWM = L_speed;
+  R_PWM = R_speed;
+
+}
---
+++ TracerSensor.cpp
@@ -0,0 +1,69 @@
+#include "TracerSensor.h"
+
+//핀 매크로
+#define PIN_IN PINC
+#define DDR_IN DDRC
+
+//사용할 센서 개수
+#define s_count 8
+
+//인터럽트에 들어갈 전역 변수
+int result[s_count] = {0,};
+int adc_now = 0;
+int adc_end = 0;
+
+//초기 설정
+void TracerInit(){
+  //ADC 포트 설정
+  DDR_IN = ~0x2F;
+  //ADMUX 초기설정
+  ADMUX = (1 << REFS0);
+  ADMUX |= 0<<MUX2 | 0<<MUX1||0<<MUX0; 
+  // ADC 사용
+  ADCSRA = 1 << ADEN; 
+  ADCSRA |= 1 << ADSC;
+  ADCSRA |= 0x20; //프리러닝
+  ADCSRA |= 1<< ADIE;
+  //ADC 프리스케일
+  ADCSRA |= (1<<ADPS2) |(0<<ADPS1) | (1<<ADPS0); 
+
+  //인터럽트 개시
+  sei();
+}
+
+//ADC 결과값 리턴
+int TracerRead(){
+
+  int way = 0;
+  for(int i = 0;i < 8;i++){
+    way <<= 1;
+    way |= result[i];
+  }
+  adc_end=0;
+  return way;
+}
+
+//ADC 인터럽트
+ISR(ADC_vect){
+  result[adc_now] = ADCW;
+
+  //들어오는 값이 500이상일 때 0
+  if(result[adc_now] > 500){
+    result[adc_now] = 0;
+  }
+  else{
+    result[adc_now] = 1;
+  }
+  
+  //ADC 7채널까지 상승하다가 7일때 0이 됨
+  if((ADMUX&0x07) == 0x07){
+    ADMUX ^= 0x07;
+    adc_now = 0;
+    adc_end = 1;
+  }
+  else{
+    ADMUX += 0x01;
+    adc_now++;
+  }
+  ADCSRA &= ~0x10;
+}
